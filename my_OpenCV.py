e025276793751abf5c785d1eccde2e013b61bb9b
聖人 <masato@seijinnoMacBook-Pro.local>
2019-01-19 19:32:43 +0900

    add HSV_convert

--- my_OpenCV.py
+++ my_OpenCV.py
@@ -108,3 +108,52 @@ def discriminant_Binarization(filename):
     tmp_img[tmp_img>=threshold]=255
 
     return tmp_img
+
+def reverse_H(filename):
+    '''
+    input->HSV->reverse 
+    return numpy.array
+    '''
+    img=imread(filename).astype(np.float32)/255
+
+    tmp_img=np.zeros_like(img)
+    Max=np.max(img,axis=2).copy()
+    Min=np.min(img,axis=2).copy()
+
+    H=np.zeros_like(Max)
+
+    H[np.where(Max==Min)]=0
+    red=img[:,:,0].copy()
+    green=img[:,:,1].copy()
+    blue=img[:,:,2].copy()
+
+    idx=np.where(np.argmin(img,axis=2)==0)
+    H[idx]=60*(blue[idx]-green[idx])/(Max[idx]-Min[idx]+1)+180
+
+    idx=np.where(np.argmin(img,axis=2)==1)
+    H[idx]=60*(red[idx]-blue[idx])/(Max[idx]-Min[idx]+1)+300
+
+    idx=np.where(np.argmin(img,axis=2)==2)
+    H[idx]=60*(green[idx]-red[idx])/(Max[idx]-Min[idx]+1)+60
+
+    V=Max.copy()
+    S=Max.copy()-Min.copy()
+    H=(H+180)%360
+
+
+    C=S
+    Hd=H//60
+    X=C*(1-np.abs(Hd%2-1))
+    Z=np.zeros_like(H)
+    vals=[[C,X,Z],[X,C,Z],[Z,C,X],[Z,X,C],[X,Z,C],[C,Z,X]]
+
+    for i in range(len(vals)):
+        idx=np.where((i<=Hd) & (Hd<(i+1)))
+        tmp_img[:,:,0][idx]=(V-C)[idx]+vals[i][0][idx]
+        tmp_img[:,:,1][idx]=(V-C)[idx]+vals[i][1][idx]
+        tmp_img[:,:,2][idx]=(V-C)[idx]+vals[i][2][idx]
+
+    tmp_img[np.where(Max==Min)]=0
+    tmp_img=(tmp_img*255).astype(np.uint8)
+    
+    return tmp_img
